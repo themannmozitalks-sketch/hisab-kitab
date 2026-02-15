66d262bbf251c44b2224c220549fba0fcb566dba
Kuldeep Singh <themannmozitalkas@gmail.com>
2026-02-15 22:14:26 +0530

    Fix main entrypoint for Render

--- app.py
+++ app.py
@@ -679,7 +679,7 @@ def invoice_pdf(invoice_id):
     )
 
 
-iif __name__ == "__main__":
+if __name__ == "__main__":
     port = int(os.getenv("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=False)
 
